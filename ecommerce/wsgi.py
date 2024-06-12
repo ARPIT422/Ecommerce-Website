b494fc495122a2415d61dffb07c13868c78fadca
ARPIT422 <bansal.arpit2001@gmail.com>
2024-06-13 01:52:29 +0530

    fix

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from vercel_wsgi import make_wsgi_handler
+#from vercel_wsgi import make_wsgi_handler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
 
